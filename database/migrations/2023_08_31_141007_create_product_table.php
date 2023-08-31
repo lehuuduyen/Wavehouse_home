80a7eea555e3bd0bd8b914993d43e49bdb05e90f
nganvu <nganvu215@gmail.com>
2023-08-31 21:23:37 +0700

    create products page

---
+++ database/migrations/2023_08_31_141007_create_product_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('code')->max(40);
+            $table->string('barcode')->max(16);
+            $table->integer('product_group_id')->max(8);
+            $table->integer('brand_id')->max(8);
+            $table->integer('position_id')->max(8);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
