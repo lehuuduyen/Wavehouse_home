84765d0fb75ff86fb3447f67e8727d86bc1d5832
nganvu <nganvu215@gmail.com>
2023-09-01 16:37:25 +0700

    update main layout

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,7 +8,7 @@
     <meta content="BQ Evaluation" property="twitter:title">
     <meta content="width=device-width, initial-scale=1" name="viewport">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
     <link href="{{ asset('assets/css/global.css') }}" rel="stylesheet" type="text/css">
     <script src="https://kit.fontawesome.com/a60e3c87cb.js" crossorigin="anonymous"></script>
 </head>
@@ -16,7 +16,7 @@
 <body>
     <div class="mainWrap-container">
         <div class="mainWrap ">
-            <header class="mainHeader clearfix  posR">
+            <header id="mainHeader" class="mainHeader clearfix posR">
                 @include('layouts.navbar')
             </header>
             <div class="container">
