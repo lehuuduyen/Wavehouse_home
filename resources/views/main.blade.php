c85158d3bb9ff14bd331f64280cc515e4f2f0dd0
admin <lehuuduyen96@gmail.com>
2023-09-04 12:18:27 +0700

    wavehousee

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -10,9 +10,13 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdn-app.kiotviet.vn/retailler/bundles/20230818050748-77ff791c/kiotviet.min.css">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrsity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
     <link href="{{ asset('assets/css/global.css') }}" rel="stylesheet" type="text/css">
-    <script src="{{ asset('assets/js/global.js') }}"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <script src="https://kit.fontawesome.com/a60e3c87cb.js" crossorigin="anonymous"></script>
+    <script src="{{ asset('assets/js/jquery-3.7.1.min.js') }}" crossorigin="anonymous"></script>
+    
 </head>
 
 <body>
